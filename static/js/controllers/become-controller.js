a688ac4b1b5989a6d70977cb15572f1290785759
Alexander <vaiol.ans@gmail.com>
2015-08-07 01:19:07 +0300

    forms logic initial

--- static/js/controllers/become-controller.js
+++ static/js/controllers/become-controller.js
@@ -1,5 +1,25 @@
 ﻿myApp.controller('become', ['$scope', '$route', function($scope, $route) {
-	 	$('ul.tabs').tabs();
-	 	$('ul.tabs').tabs('select_tab', 'test1');
- 
+	$scope.submit=function(fighter){
+		var m = {}
+		m = angular.copy(fighter);
+		console.log(m);
+		console.log(m.info);
+
+        alert(angular.toJson(m));
+    }
+
+    $scope.updateFields = function() {
+    	var bool1 = $scope.FighterForm.fighterPhone.$error.minlength;
+    	var bool2 = angular.element(fp).hasClass("active");
+    	// console.log(bool2);
+    	// console.log(bool1);
+    	var bool3 = (bool1 && bool2);
+    	console.log(bool3);
+    	return bool3;
+    }
 }]);
+
+
+// myApp.controller('fighterFormController', ['$scope',  function($scope) {
+	
+// }]);
