45110ad9bd9456a32d88fe4d9f70e70e3124c2c0
Alexander <vaiol.ans@gmail.com>
2015-08-08 21:56:49 +0300

    editor demo

--- static/js/controllers/help-controller.js
+++ static/js/controllers/help-controller.js
@@ -1,3 +1,22 @@
 ﻿myApp.controller('help', ['$scope', function($scope) {
+	var editor = new Quill('#editor-container', {
+        modules: {
+          'toolbar': { container: '#toolbar-container' },
+          'link-tooltip': true,
+          'image-tooltip': true
+        },
+        theme: 'snow'
+      });
+
+      function editorTextToHTML()
+      {
+          document.getElementById("editor-html-text").value  = editor.getHTML()
+          document.getElementById("editor-html-view").innerHTML = editor.getHTML()
+      }
+      editor.on('text-change', function(delta, source) {
+          if (source === 'user') {
+              editorTextToHTML()
+          }
+      });
 
 }]);
